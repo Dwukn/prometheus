854bf6f62ef0169c3e08fc5caf9054897a5b3024
Dawood Khan <152021471+Dwukn@users.noreply.github.com>
2024-12-27 00:23:41 +0530

    completed playfair.go

--- playfair.go
+++ playfair.go
@@ -43,3 +43,58 @@ matrix := createPlayfairMatrix(key)
 text = strings.ToUpper(text)
 text = strings.ReplaceAll(text, "J", "I")
 // Split text into digraphs
+  var digraphs []string
+for i := 0; i < len(text); i++ {
+if i+1 < len(text) && text[i] != text[i+1] {
+digraphs = append(digraphs, string(text[i:i+2]))
+i++
+} else {
+digraphs = append(digraphs, string(text[i])+"X")
+}
+}
+// Encrypt or decrypt based on mode
+for _, digraph := range digraphs {
+a, b := digraph[0], digraph[1]
+var (row1, col1, row2, col2 int)
+// Find positions of the letters
+for i := 0; i < 5; i++ {
+for j := 0; j < 5; j++ {
+if matrix[i][j] == rune(a) {
+row1, col1 = i, j
+}
+if matrix[i][j] == rune(b) {
+row2, col2 = i, j
+}
+}
+}
+if row1 == row2 {
+if mode == "encrypt" {
+result.WriteRune(matrix[row1][(col1+1)%5])
+result.WriteRune(matrix[row2][(col2+1)%5])
+} else {
+result.WriteRune(matrix[row1][(col1-1+5)%5])
+result.WriteRune(matrix[row2][(col2-1+5)%5])
+}
+} else if col1 == col2 {
+if mode == "encrypt" {
+result.WriteRune(matrix[(row1+1)%5][col1])
+result.WriteRune(matrix[(row2+1)%5][col2])
+} else {
+result.WriteRune(matrix[(row1-1+5)%5][col1])
+result.WriteRune(matrix[(row2-1+5)%5][col2])
+}
+} else {
+result.WriteRune(matrix[row1][col2])
+result.WriteRune(matrix[row2][col1])
+}
+}
+return result.String()
+}
+func main() {
+text := "HELLO WORLD"
+key := "KEYWORD"
+enc := playfairCipher(text, key, "encrypt")
+dec := playfairCipher(enc, key, "decrypt")
+fmt.Println("Playfair Cipher Encrypt:", enc)
+fmt.Println("Playfair Cipher Decrypt:", dec)
+}
