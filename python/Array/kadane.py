66142c5fe4ec2de11bd9778d58cf4a5ce95b8fd2
Dawood <dawood220a+github.com>
2025-01-01 23:18:21 +0530

    kadane-problem

---
+++ python/Array/kadane.py
@@ -0,0 +1,14 @@
+def maxSubarraySum(res):
+    maxSum = res[0]
+    currentSum = res[0]
+    for i in range(1, len(res)):
+        currentSum = max(res[i], currentSum + res[i])
+        maxSum = max(maxSum, currentSum)
+    return maxSum
+
+def main():
+    res = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print("Maximum Subarray Sum:", maxSubarraySum(res))
+
+if __name__ == "__main__":
+    main()
---
+++ python/test.py
@@ -0,0 +1,3 @@
+arr = [0, 1, 2, 3, 4]
+arr.insert(2, 9)
+print(arr)
