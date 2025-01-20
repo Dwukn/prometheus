d3d589d5b168d2b712ef8f927bb89190ae0a13c0
Dawood <dawood220a+github@gmail.com>
2025-01-21 00:30:14 +0530

    Completed Configs for User and added a quick edit script

--- configs/hypr/ScriptsUsers/QuickEdit.sh
+++ configs/hypr/ScriptsUsers/QuickEdit.sh
@@ -13,37 +13,38 @@ UserConfigs="$HOME/.config/hypr/UserConfigs"
 # Function to display the menu options
 menu() {
     cat <<EOF
-1. Edit Env-variables
-2. Edit Window-Rules
-3. Edit Startup_Apps
-4. Edit User-Keybinds
-5. Edit Monitors
-6. Edit Laptop-Keybinds
-7. Edit User-Settings
-8. Edit Decorations & Animations
-9. Edit Workspace-Rules
-10. Edit Default-Settings
-11. Edit Default-Keybinds
+1. Edit Decorations & Animations
+2. Edit Keybinds
+3. Edit System - Keybinds
+4. Edit Monitors
+5. Edit laptop configs
+6. Edit Environment variables
+7. Edit Startup_Apps
+8. Edit User-Settings
+9. Edit Window Rules
+10. Edit Work-space Rules
 EOF
 }
 
 # Main function to handle menu selection
 main() {
-    choice=$(menu | rofi -i -dmenu -config ~/.config/rofi/config-compact.rasi | cut -d. -f1)
+    # Display the menu with rofi and allow multi-column layout
+    choice=$(menu | rofi -i -dmenu -config ~/.config/rofi/config-compact.rasi -columns 2 | cut -d. -f1)
 
     # Map choices to corresponding files
     case $choice in
-        1) file="$UserConfigs/ENVariables.conf" ;;
-        2) file="$UserConfigs/WindowRules.conf" ;;
-        3) file="$UserConfigs/Startup_Apps.conf" ;;
-        4) file="$UserConfigs/UserKeybinds.conf" ;;
-        5) file="$UserConfigs/Monitors.conf" ;;
-        6) file="$UserConfigs/Laptops.conf" ;;
-        7) file="$UserConfigs/UserSettings.conf" ;;
-        8) file="$UserConfigs/UserDecorAnimations.conf" ;;
+    # left side
+        1) file="$UserConfigs/Animations.conf" ;;
+        2) file="$configs/Keybinds.conf" ;;
+        3) file="$UserConfigs/UserKeybinds.conf" ;;
+        4) file="$UserConfigs/Monitors.conf" ;;
+        5) file="$UserConfigs/Laptops.conf" ;;
+        # right side
+        6) file="$UserConfigs/EnvVariables.conf" ;;
+        7) file="$UserConfigs/Startup_Apps.conf" ;;
+        8) file="$UserConfigs/UserSettings.conf" ;;
         9) file="$UserConfigs/WorkspaceRules.conf" ;;
         10) file="$configs/Settings.conf" ;;
-        11) file="$configs/Keybinds.conf" ;;
         *) return ;;  # Do nothing for invalid choices
     esac
 
