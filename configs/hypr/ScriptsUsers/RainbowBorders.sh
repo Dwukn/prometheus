5cb2969898a14c887c4fa0d58e860d08bb3998cf
Dawood <dawood220a+github@gmail.com>
2025-01-21 23:47:32 +0530

    Random wallpaper

--- configs/hypr/ScriptsUsers/RainbowBorders.sh
+++ configs/hypr/ScriptsUsers/RainbowBorders.sh
@@ -2,13 +2,19 @@
 # /* ---- 💫 https://github.com/Dwukn 💫 ---- */  ##
 # for rainbow borders animation
 
-function random_hex() {
-    random_hex=("0xff$(openssl rand -hex 3)")
-    echo $random_hex
+# Function to generate a random hex color
+random_hex() {
+    printf "0xff%s" "$(openssl rand -hex 3)"
 }
 
-# rainbow colors only for active window
-hyprctl keyword general:col.active_border $(random_hex)  $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex)  270deg
+# Generate 10 random rainbow colors
+colors=()
+for i in {1..10}; do
+    colors+=("$(random_hex)")
+done
+
+# Set the active window border colors using the generated random colors
+hyprctl keyword general:col.active_border "${colors[@]}" 270deg
 
 # rainbow colors for inactive window (uncomment to take effect)
 #hyprctl keyword general:col.inactive_border $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) $(random_hex) 270deg
